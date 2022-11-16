d19413e1ad2d39250033d042fa2442fca2558dc6
Professor-development <Professor.spb.47@gmail.com>
2022-11-17 00:23:35 +0300

    version-0.2

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self, posts_rate, comments_rate, comments_posts_rate):
+        self.rating = posts_rate * 3 + comments_rate + comments_posts_rate
+        self.save()
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=255, unique=True)
+
+
+type_of_posts_choice = [
+    ('ST', 'статья'),
+    ('NE', 'новость')
+]
+
+
+class Post(models.Model):
+    article = 'ST'
+    news = 'NW'
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    type_of_post = models.CharField(max_length=2, choices=type_of_posts_choice)
+    time_of_publication = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    header = models.CharField(max_length=255)
+    text = models.TextField()
+    rate = models.IntegerField(default=0)
+
+    def like(self):
+        self.rate += 1
+        self.save()
+
+    def dislike(self):
+        self.rate -= 1
+        self.save()
+
+    def preview(self):
+        try:
+            preview_text = self.text[0:124] + '...'
+        except IndexError:
+            preview_text = self.text
+        return preview_text
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_of_public = models.DateTimeField(auto_now_add=True)
+    rate = models.IntegerField(default=0)
+
+    def like(self):
+        self.rate += 1
+        self.save()
+
+    def dislike(self):
+        self.rate -= 1
+        self.save()
