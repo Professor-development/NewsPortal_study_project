eb927d4baa326a7ebb1d3d7e11ede7c11e435907
Professor-development <Professor.spb.47@gmail.com>
2022-11-26 20:13:18 +0300

    version 0.4

--- NewsPortal/news/models.py
+++ NewsPortal/news/models.py
@@ -6,6 +6,9 @@ class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
 
+    def get_username(self):
+        return
+
     def update_rating(self, posts_rate, comments_rate, comments_posts_rate):
         self.rating = posts_rate * 3 + comments_rate + comments_posts_rate
         self.save()
@@ -26,6 +29,7 @@ class Post(models.Model):
     news = 'NW'
 
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     type_of_post = models.CharField(max_length=2, choices=type_of_posts_choice)
     time_of_publication = models.DateTimeField(auto_now_add=True)
     category = models.ManyToManyField(Category, through='PostCategory')
@@ -49,7 +53,7 @@ class Post(models.Model):
         return preview_text
 
     def __str__(self):
-        return f"{self.header}\n{self.text}"
+        return f"{self.header}\n{self.text}\nАвтор: {self.user}"
 
 
 class PostCategory(models.Model):
---
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+CENSOR_WORDS = {'хребет', 'дроны', 'массы', 'тибет'}
+
+
+@register.filter()
+def censor(value):
+    final_text = ""
+    value = str(value)
+    if isinstance(value, str):
+        counter = 0
+        while counter < len(value):
+            i = counter
+            temp_word = ''
+            if value[i] != ' ':
+                while i < len(value) and value[i] != ' ':
+                    temp_word += value[i]
+                    i += 1
+                if temp_word.lower() in CENSOR_WORDS:
+                    temp_word = temp_word[0] + ('*' * (len(temp_word)-1))
+                final_text += temp_word + " "
+            counter = i + 1
+        return final_text
+    else:
+        raise ValueError("Невозможно применить фильтр к не строковому типу данных")
